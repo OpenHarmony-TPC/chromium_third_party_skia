9ffb80ecf5ebe99f45f332c1ef45717f4dcec368
wangshengwen333 <wangshengwen4@h-partners.com>
2025-10-20 21:51:04 +0800

    Strike问题兜底逻辑合入蓝区商分
    Signed-off-by: wangshengwen333 <wangshengwen4@h-partners.com>

--- src/core/SkStrikeCache.cpp
+++ src/core/SkStrikeCache.cpp
@@ -251,7 +251,6 @@ size_t SkStrikeCache::internalPurge(size_t minBytesNeeded, bool checkPinners) {
     #if BUILDFLAG(IS_ARKWEB)
         if (strike->fRemoved) {
             LOG(ERROR) << "strike is already removed or invalid strike pointer";
-            continue;
         }
     #endif
             this->internalRemoveStrike(strike);
