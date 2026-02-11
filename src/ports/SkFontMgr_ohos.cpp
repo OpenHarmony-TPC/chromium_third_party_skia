9b01dfd5f11935605ec7a579306bbba50c971b66
caizhiwei <caizhiwei7@huawei.com>
2026-02-11 10:10:20 +0800

    【功能-BUGFIX】修复字体配置信息未销毁导致内存泄露的问题
    Signed-off-by: Blake <zhenlong.zhu@haitaichina.com>
    Change-Id: Ieae89db542b7d2c6db168194cda5fd73fd6cd648

--- src/ports/SkFontMgr_ohos.cpp
+++ src/ports/SkFontMgr_ohos.cpp
@@ -283,7 +283,7 @@ uint32_t SkFontStyleSet_OHOS::getFontStyleDifference(const SkFontStyle& dstStyle
         }
     }
     // The first 2 bytes to save weight difference, the third byte to save slant difference,
-    // and the fourth byte to save width difference
+    // and the fourth byte to save width difference 
     uint32_t diff = (widthDiff << 24) + (slantDiff << 16) + weightDiff;
     return diff;
 }
